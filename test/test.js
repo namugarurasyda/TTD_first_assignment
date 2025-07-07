5367c28c686813475780435b4a943e1d7d9b1a07
namugarurasyda <2100813912@std.kyu.ac.ug>
2025-07-07 10:39:07 +0300

    First multiplication

---
+++ test/test.js
@@ -0,0 +1,14 @@
+import {assert} from 'chai';
+
+describe('multiply the  number', function(){
+
+   it('Check the first multiplication ', function(){
+        assert.equal(multiply(1,1), 1)
+     })
+    
+
+});
+
+
+
+
