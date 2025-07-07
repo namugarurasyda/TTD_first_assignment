5d576e79b5cb6f50cc3ba202f483ab7dd0dfab6c
Conrad Waako K <68024054+elconrad23@users.noreply.github.com>
2025-07-07 23:25:17 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,17 @@
 import {assert} from 'chai';
-
+//the describe block is a test suite
 describe('multiply the  number', function(){
 
+   function multiply(a, b){
+      return a*b;
+   }
+   //should run if multiplication of the function parameters gives a coorect answer (1)
+   //the function should be defined as well
    it('Check the first multiplication ', function(){
         assert.equal(multiply(1,1), 1)
      }),
 
+   //'it' function contains the function and expected return value to verify 
    it('Check the second multiplication ', function(){
         assert.equal(multiply(3,3), 9)
      })
