4adc488fe5eb2136d9f9d1c087b224f6dd221c90
NAMUGARURA SYDA <138879820+namugarurasyda@users.noreply.github.com>
2025-07-07 11:16:45 +0300

    second multiplication

--- test/test.js
+++ test/test.js
@@ -4,6 +4,10 @@ describe('multiply the  number', function(){
 
    it('Check the first multiplication ', function(){
         assert.equal(multiply(1,1), 1)
+     }),
+
+   it('Check the second multiplication ', function(){
+        assert.equal(multiply(3,3), 9)
      })
     
 
